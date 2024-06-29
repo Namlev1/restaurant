b3c5592345683d240ef9d66e7f4de8bb00912ade
namlev <mikipatkowski@protonmail.com>
2024-06-29 16:00:26 +0200

    Implement hero image

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './styles.css'
+import Icon from './hero.jpg'
+
+const hero = document.querySelector('#hero');
+const imgContainer = document.createElement('div');
+imgContainer.id = ('hero-img-container');
+const img = document.createElement('img')
+img.src = Icon;
+img.alt = "Food picture"
+imgContainer.appendChild(img)
+hero.appendChild(imgContainer);
 console.log('loggin\'');
